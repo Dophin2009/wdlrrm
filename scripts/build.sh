00b4baf0e2d16b08115091d87b2e5dd763822c56
Eric Zhao <21zhaoe@protonmail.com>
2020-02-13 16:13:01 -0500

    Fix rmarkdown dependency check

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@ script_dir=scripts
 script_checkcmd=$script_dir/check_cmd.sh
 
 # Check if required commands exist
-declare -a req_commands=("sassc" "xelatex" "pdftoppm")
+declare -a req_commands=("sassc" "xelatex" "pandoc" "pdftoppm")
 for c in "${req_commands[@]}"; do
   $script_checkcmd "$c" || exit
 done
--- scripts/render.R
+++ scripts/render.R
@@ -1,3 +1,11 @@
 #!/bin/Rscript
+req_packages <- c("rmarkdown", "revealjs")
+for (pkg in req_packages) {
+  if (!require(pkg, character.only = TRUE)) {
+    install.packages(pkg, dependencies = TRUE)
+    require(pkg, character.only = TRUE)
+  }
+}
+
 rmarkdown::render("index.Rmd", "revealjs::revealjs_presentation",
                   output_file = "../index.html")
