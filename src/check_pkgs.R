6a61f7a3434655f1f56ee736962851e76cf733e1
Eric Zhao <21zhaoe@protonmail.com>
2020-02-16 23:21:04 -0500

    Finish up R markdown slides

--- src/check_pkgs.R
+++ src/check_pkgs.R
@@ -1,4 +1,4 @@
-req_packages <- c("readr", "hashmap")
+req_packages <- c("readr", "hashmap", "reticulate")
 for (pkg in req_packages) {
   if (!require(pkg, character.only = TRUE)) {
     install.packages(pkg, dependencies = TRUE)
